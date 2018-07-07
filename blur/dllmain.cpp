3dcc3027f94fa7b7c64f7d7e50b48fcb90b22894
BrunoBM16 <brunobmello2017@gmail.com>
2018-07-07 20:50:43 -0300

    A few changes

--- blur/dllmain.cpp
+++ blur/dllmain.cpp
@@ -2,27 +2,25 @@
 
 void Thread()
 {
-	while (!*(int*)0xB6F5F0) Sleep(50);
-	
-	DWORD OldProtect;
+	while (!*reinterpret_cast<int*>(0xB6F5F0)) Sleep(50);
 
-	VirtualProtect((unsigned char*)0x704E8A, 5, PAGE_EXECUTE_READWRITE, &OldProtect);
+	DWORD OldProtect = NULL;
 
-	*(unsigned char*)0x704E8A = 0xE8;
-	*(unsigned char*)0x704E8B = 0x11; 
-	*(unsigned char*)0x704E8C = 0xE2;
-	*(unsigned char*)0x704E8D = 0xFF;
-	*(unsigned char*)0x704E8E = 0xFF;
+	const BYTE byte_array[] = {0xE8, 0x11, 0xE2, 0xFF, 0xFF};
+
+	VirtualProtect(reinterpret_cast<void*>(0x704E8A), 5, PAGE_EXECUTE_READWRITE, &OldProtect);
+
+	memcpy(reinterpret_cast<void*>(0x704E8A), byte_array, 5);
 
 	if (fexists("blur.ini"))
 	{
-		*(unsigned char*)0x8D5104 = (unsigned char)GetPrivateProfileInt("Settings", "BlurLevel", 36, "./blur.ini");
+		*reinterpret_cast<BYTE*>(0x8D5104) = GetPrivateProfileInt("Settings", "BlurLevel", 36, "./blur.ini");
 	}
 	else
 	{
 		WritePrivateProfileString("Settings", "BlurLevel", "36", "./blur.ini");
 
-		*(unsigned char*)0x8D5104 = 0x24;
+		*reinterpret_cast<BYTE*>(0x8D5104) = 0x24;
 	}
 }
 
@@ -31,7 +29,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
 	{
 		DisableThreadLibraryCalls(hModule);
-		CreateThread(0, 0, (LPTHREAD_START_ROUTINE)Thread, 0, 0, 0);
+		CreateThread(0, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(Thread), 0, 0, 0);
 	}
 	return 1;
 }
